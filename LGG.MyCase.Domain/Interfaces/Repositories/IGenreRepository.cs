e8758ecabab50b621e8a00acf6339750a3dbb772
leletronic <leandru@gmail.com>
2015-08-26 19:22:57 -0300

    Creating Infra project with some classes related.

---
+++ LGG.MyCase.Domain/Interfaces/Repositories/IGenreRepository.cs
@@ -0,0 +1,15 @@
+﻿using LGG.MyCase.Domain.Models;
+using System;
+using System.Collections.Generic;
+
+namespace LGG.MyCase.Domain.Interfaces.Repositories
+{
+    public interface IGenreRepository : IDisposable
+    {
+        IEnumerable<Genre> GetGenres();
+        Genre GetGenre(int id);
+        void Create(Genre genre);
+        void Update(Genre genre);
+        void Delete(Genre genre);
+    }
+}
